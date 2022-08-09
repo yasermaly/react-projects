96b9a94161bea339fca2b92204b7c906a971b45f
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-08-09 15:39:50 -0700

    tours app working

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,10 +3,16 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+const removeTour = (id) => {
+  const newTours = tours.filter((tour) => tour.id !== id);
+  setTours(newTours);
+}
 
 const fetchTours = async () => {
   setLoading(true);
@@ -33,9 +39,21 @@ useEffect(() => {
       </main>
     );
   }
+
+if (tours.length === 0) {
   return (
     <main>
-      <Tours tours={tours} />
+      <div>
+        <h2>no tours left</h2>
+        <button className="btn" onClick={() => fetchTours()}>refresh</button>
+      </div>
+    </main>
+  )
+}
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour= {removeTour}/>
     </main>
     )
 }
