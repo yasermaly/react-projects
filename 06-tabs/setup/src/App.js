487ece24426812bc0eb9dbda4949ba7158ca4c04
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-08-19 16:13:54 -0700

    tabs project done

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -39,7 +39,11 @@ function App() {
           {
             jobs.map((item, index) => {
               return (
-                <button key={item.id} onClick={() => setValue(index)}>{item.company}</button>
+                <button key={item.id} 
+                        onClick = { () => setValue(index) }
+                        className = {`job-btn ${index === value && 'active-btn'}`}>
+                  {item.company}
+                </button>
               )
             })
           }
